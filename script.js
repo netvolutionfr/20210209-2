d7bac0ba33800a2497cf5b33085674ff1f02a715
Stanis Humez <stanis@netvolution.fr>
2021-02-09 08:58:50 +0100

    Controle sur le champ nom

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 console.log("Script lancé");
 
 saisie = document.getElementById("saisie");
-saisie.focus();
-
 champnom = document.getElementById("nom");
+
+champnom.focus();
+
 
 bouton = document.getElementById("bouton");
 bouton.addEventListener("click", function() {
+    // Expression régulière ou "Regexp" :
+    const texteAccepte = /^[A-Z]+$/;
     const valeursAcceptees = /^[0-9]+$/;
-    if (saisie.value.length === 0) {
+
+    if (saisie.value.length === 0 || champnom.value.length === 0) {
         alert("Saisie vide");
     } else {
-        if (valeursAcceptees.test(saisie.value)) {
-            alert("Votre saisie : " + saisie.value);
+        if (valeursAcceptees.test(saisie.value) && texteAccepte.test(champnom.value)) {
+            alert("Votre saisie : " + saisie.value + " / nom : " + champnom.value);
         } else {
-            alert("Vous n'avez pas saisi un nombre");
+            alert("Saisie incorrecte");
         }
     }
 });
